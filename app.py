1c6e386e867b26f500bec273d04a591db1e20a57
Dan Nguyen <dansonguyen@gmail.com>
2016-05-09 06:23:39 -0700

    first

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
